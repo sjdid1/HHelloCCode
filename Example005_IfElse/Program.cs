b67605e159e8c6520a52ae08dd6a972a147e789d
Ilia <shishmagaevi@mail.ru>
2023-01-12 13:09:40 +0300

    Example 005 if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "илья")
+{
+    Console.WriteLine("Ура, это же лучший в мире");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
